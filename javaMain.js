aeac3c0b44be3ecc516ff6e930904c44de0d8f28
samane ghodrati <samane.ghodrati@gmail.com>
2025-02-18 00:33:43 -0900

    add

--- javaMain.js
+++ javaMain.js
@@ -2,11 +2,15 @@ const pstartEl=document.querySelector("pStart");
 const penadEl=document.querySelector('pEnd');
 let timein=3;
 let timesec=0;
-let number=8;
+let number=10;
 pstartEl.textContent=`Remaining Guesses${number}`; // تعداد دفعات 
 penadEl.textContent=`time left:${timein}:${timesec}` ;// زمان
 setInterval(() => {
-    if(timein<=0&&timein>0){
+    if(timein<=10&&timein<0){
+     timein--;
+     penadEl.style.color="red";
+    }
+    else if(timein<=0&&timein>0){
      timein--;
      timesec=59;
     }else if(timein>0&&timein>0){
